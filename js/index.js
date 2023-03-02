1487953d8ff2b5ee05a28e738f6a3a863a45a920
ke1kyu <105688133+ke1kyu@users.noreply.github.com>
2023-03-02 19:37:59 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var sum = 0; // 計算結果を保持する変数
+
+// inputの値が変更された時
+function inputCheck() {
+    var inputMin = Number(document.getElementById("min").value);
+    var inputSec = Number(document.getElementById("sec").value);
+    var inputFps = Number(document.getElementById("fps").value);
+
+    sum = (inputMin * 60 + inputSec) * inputFps;
+
+    document.getElementById("result").innerHTML = '計算結果 = ' + sum;
+}
+
+// コピーボタンを押下した時
+function copy(){
+    navigator.clipboard.writeText(sum);
+}
